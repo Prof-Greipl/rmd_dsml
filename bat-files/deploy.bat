e7d7ed8288b2fe49605f3d48acd7ea78efee5a6e
Prof-Greipl <dieter.greipl@haw-landshut.de>
2022-02-02 18:59:54 +0100

    from deploy.bat

--- bat-files/deploy.bat
+++ bat-files/deploy.bat
@@ -1,12 +1,16 @@
 @echo off
-xcopy ..\_book ..\docs /s /e /y
-cd ..\docs
+REM rende the book
+cd ..
+rem "C:\Program Files\R\R-4.1.2\bin\R.exe" -e "rmarkdown::render_site(output_format = 'bookdown::bs4_book', encoding = 'UTF-8')"
+
+xcopy _book docs /s /e /y
 
 REM Delete all unused files
+cd docs
 del *.pdf
 del *.md
 del *.tex
-
 cd ..
+
 git add -A && git commit -m "from deploy.bat"
 git push
