26549863f5b78c42d664b7379b3ea19bd3d86b18
Prof-Greipl <dieter.greipl@haw-landshut.de>
2022-05-29 16:41:05 +0200

    Online, vor dem Termin am 29.05.2022

---
+++ clean.bat
@@ -0,0 +1,9 @@
+@echo off
+cd "C:\Users\diete\OneDrive - Fachhochschule Landshut\05 GitHub-Pages-Publish\bookdown\dsml"
+rmdir /s /q "_bookdown_files"
+rmdir /s /q "06-visualisierung-plt_files"
+rmdir /s /q "07-visualisierung-sea_files"
+rmdir /s /q "08-visualisierung-iris_files"
+rmdir /s /q "15-train-test-val_files"
+
+pause
